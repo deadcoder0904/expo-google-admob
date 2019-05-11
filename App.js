3dd221adce62cb8b5caec6d92ac57541d4e80929
Akshay Kadam <akshaykadam0904@gmail.com>
2019-05-11 11:20:22 +0530

    Reduce 30 LOCs by using UI library

--- App.js
+++ App.js
@@ -1,93 +1,62 @@
-import { AdMobBanner, AdMobInterstitial, AdMobRewarded, PublisherBanner } from "expo";
-import React, { Component } from "react";
-import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  AdMobBanner,
+  AdMobInterstitial,
+  AdMobRewarded,
+  PublisherBanner,
+} from 'expo'
+import React, { Component } from 'react'
+import { SafeAreaView } from 'react-native'
+import { Button, Text } from 'react-native-elements'
 
-const ADUNITID = `ca-app-pub-1425926517331745~6816357585`;
-const BANNER_ID = `ca-app-pub-1425926517331745/4139536433`;
-const INTERSTITIAL_ID = `ca-app-pub-1425926517331745/1141181467`;
-const REWARDED_ID = `ca-app-pub-1425926517331745/3923257478`;
+const ADUNITID = `ca-app-pub-1425926517331745~6816357585`
+const BANNER_ID = `ca-app-pub-1425926517331745/4139536433`
+const INTERSTITIAL_ID = `ca-app-pub-1425926517331745/1141181467`
+const REWARDED_ID = `ca-app-pub-1425926517331745/3923257478`
 
-AdMobInterstitial.setAdUnitID(INTERSTITIAL_ID);
-AdMobInterstitial.setTestDeviceID("EMULATOR");
-AdMobRewarded.setAdUnitID(REWARDED_ID);
-AdMobRewarded.setTestDeviceID("EMULATOR");
-console.disableYellowBox = true;
+AdMobInterstitial.setAdUnitID(INTERSTITIAL_ID)
+AdMobInterstitial.setTestDeviceID('EMULATOR')
+AdMobRewarded.setAdUnitID(REWARDED_ID)
+AdMobRewarded.setTestDeviceID('EMULATOR')
 
 class App extends Component {
   _openInterstitial = async () => {
-    await AdMobInterstitial.requestAdAsync();
-    await AdMobInterstitial.showAdAsync();
-  };
+    await AdMobInterstitial.requestAdAsync()
+    await AdMobInterstitial.showAdAsync()
+  }
 
   _openRewarded = async () => {
-    await AdMobRewarded.requestAdAsync();
-    await AdMobRewarded.showAdAsync();
-  };
+    await AdMobRewarded.requestAdAsync()
+    await AdMobRewarded.showAdAsync()
+  }
 
   render() {
     return (
-      <ScrollView>
-        <View style={styles.container}>
-          <Text style={styles.title}>GOOGLE ADMOB DEMO</Text>
-          <Text style={styles.text}>
-            Set Ad Unit Id, Interstitial Id & Rewarded Id only on the top level
-            component once.
-          </Text>
-          <Text style={styles.title}>BANNER AD</Text>
-          <AdMobBanner
-            bannerSize="fullBanner"
-            adUnitID={BANNER_ID}
-            didFailToReceiveAdWithError={this.bannerError}
-          />
-
-          <Text style={styles.title}>PUBLISHER BANNER</Text>
-          <PublisherBanner
-            bannerSize="banner"
-            adUnitID={BANNER_ID}
-            onDidFailToReceiveAdWithError={this.bannerError}
-            onAdMobDispatchAppEvent={this.adMobEvent}
-          />
-          <View style={styles.btn}>
-            <Text style={styles.title}>INTERSTITIAL AD</Text>
-            <Button
-              title="OPEN"
-              color="steelblue"
-              onPress={this._openInterstitial}
-            />
-          </View>
-          <View style={styles.btn}>
-            <Text style={styles.title}>REWARDED AD</Text>
-            <Button title="OPEN" color="green" onPress={this._openRewarded} />
-          </View>
-        </View>
-      </ScrollView>
-    );
+      <SafeAreaView style={{ margin: 20 }}>
+        <Text h2>GOOGLE ADMOB DEMO</Text>
+        <Text>
+          Set Ad Unit Id, Interstitial Id & Rewarded Id only on the top level
+          component once.
+        </Text>
+        <Text h4>Banner Ad</Text>
+        <AdMobBanner
+          bannerSize="fullBanner"
+          adUnitID={BANNER_ID}
+          didFailToReceiveAdWithError={this.bannerError}
+        />
+        <Text h4>Publisher Banner</Text>
+        <PublisherBanner
+          bannerSize="banner"
+          adUnitID={BANNER_ID}
+          onDidFailToReceiveAdWithError={this.bannerError}
+          onAdMobDispatchAppEvent={this.adMobEvent}
+        />
+        <Text h4>Interstitial Ad</Text>
+        <Button title="Open" type="outline" onPress={this._openInterstitial} />
+        <Text h4>Rewarded Ad</Text>
+        <Button title="Open" type="outline" onPress={this._openRewarded} />
+      </SafeAreaView>
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    marginTop: 30
-  },
-  btn: {
-    margin: 7
-  },
-  text: {
-    textAlign: "center",
-    fontSize: 24,
-    color: "#000",
-    backgroundColor: "#eee333",
-    padding: 10,
-    margin: 10
-  },
-  title: {
-    marginTop: 10,
-    fontSize: 30,
-    color: "#333"
-  }
-});
-
-export default App;
+export default App
